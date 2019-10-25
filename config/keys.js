c213861b4ff2a7fe0f4f1dd715d6c5ecc8b28b76
pratimakarki2014 <pratima.karki.2014@gmail.com>
2019-10-25 16:18:21 -0400

    Detabase connection done

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //MongoDB: 'mongodb://onlinedatingapp:HelloWorld1234@onlinedatingapp-l0rhj.mongodb.net/test?retryWrites=true&w=majority'
+    MongoDB: "mongodb://onlinedatingapp:HelloWorld1234@onlinedatingapp-shard-00-00-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-01-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-02-l0rhj.mongodb.net:27017/test?ssl=true&replicaSet=OnlineDatingApp-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+};
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//Load models
+const Message = require('./models/message');
 const app = express();
+//load keys file
+const keys = require('./config/keys');
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+//connect to mLab MongoDB
+mongoose.connect(keys.MongoDB,{useNewUrlParser:true}).then(() => {
+    console.log('Server is connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 //environment var for port
 const port = process.env. PORT || 3000;
 //setup view engine
@@ -23,6 +38,10 @@ app.get('/contact',(req,res) => {
     res.render('contact',{
         title: 'Contact'
     });
+});
+
+app.post('/contactUs',(req,res) => {
+    console.log(req.body);
 });
 
 app.listen(port,() => {
