f557b8d846f056cacdfaf670a4f58e5dabbc1481
pratimakarki2014 <pratima.karki.2014@gmail.com>
2019-10-30 17:26:16 -0400

    second commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     //MongoDB: 'mongodb://onlinedatingapp:HelloWorld1234@onlinedatingapp-l0rhj.mongodb.net/test?retryWrites=true&w=majority'
-    MongoDB: "mongodb://onlinedatingapp:HelloWorld1234@onlinedatingapp-shard-00-00-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-01-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-02-l0rhj.mongodb.net:27017/test?ssl=true&replicaSet=OnlineDatingApp-shard-0&authSource=admin&retryWrites=true&w=majority"
-
+    MongoDB: "mongodb://onlinedatingapp:HelloWorld1234@onlinedatingapp-shard-00-00-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-01-l0rhj.mongodb.net:27017,onlinedatingapp-shard-00-02-l0rhj.mongodb.net:27017/test?ssl=true&replicaSet=OnlineDatingApp-shard-0&authSource=admin&retryWrites=true&w=majority",
+    FacebookAppID: '499931033894047',
+    FacebookAppSecret: 'ae92ba1c8cc56065fe539db3cccdb5cc'
 };
--- server.js
+++ server.js
@@ -2,20 +2,38 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 //Load models
 const Message = require('./models/message');
+const User = require('./models/user');
 const app = express();
 //load keys file
 const keys = require('./config/keys');
 //use body parser middleware
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+//confirguration for authentication
+app.use(cookieParser());
+app.use(session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//load facebook strategy
+require('./passport/facebook');
 //connect to mLab MongoDB
 mongoose.connect(keys.MongoDB,{useNewUrlParser:true}).then(() => {
     console.log('Server is connected to MongoDB');
 }).catch((err) => {
     console.log(err);
 });
+
 //environment var for port
 const port = process.env. PORT || 3000;
 //setup view engine
@@ -40,8 +58,40 @@ app.get('/contact',(req,res) => {
     });
 });
 
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook',{
+    successRedirect:'/profile',
+    failureRedirect: '/'
+}));
+
 app.post('/contactUs',(req,res) => {
     console.log(req.body);
+    const newMessage = {
+        fullname: req.body.fullname,
+        email: req.body.email,
+        message: req.body.message,
+        date: new Date()
+    };
+
+    new Message(newMessage).save((err, message) => {
+        if (err) {
+            throw err;
+        }else{
+            Message.find({}).then((messages) => {
+                if (messages) {
+                    res.render('newmessage', {
+                        title: 'Sent',
+                        messages: messages
+                    });
+                }else{
+                    res.render('noMessage',{
+                        title: 'Not Found',
+                        messages: "Sorry no message found"
+                    });
+                }
+            })
+        }
+    });
 });
 
 app.listen(port,() => {
